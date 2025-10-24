5912adf1a997e2bfe993dc3d6a68361da51e8534
Kawona <ojekaona@gmail.com>
2025-10-24 10:51:43 +0000

    Added _document.tsx to the file

--- alx-movie-app/next.config.mjs
+++ alx-movie-app/next.config.mjs
@@ -1,18 +1,18 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
-
 const withPWA = withPWAInit({
-  dest: 'public'
+  dest: "public",
 });
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['m.media-amazon.com'],
+    domains: ["m.media-amazon.com"],
   },
+  turbopack: {}, // 👈 Added to silence Turbopack warning
 };
 
 export default withPWA({
-  ...nextConfig
+  ...nextConfig,
 });
--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* This line links your PWA manifest file */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* Defines your browser theme color (for address bar and splash screen) */}
+        <meta name="them-color" content="#0070f3" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
